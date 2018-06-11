4eacfe2cd017f1f958ff8c5d812e5e1d6e8ddb9d
Bigjek <ivapira@mail.ru>
2018-06-11 17:37:05 +0300

    start pack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+    },
+    mode: 'development'
+}
