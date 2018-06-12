7c234ec12553fb3b51575995ace19ccaf18cb4bf
Bigjek <ivapira@mail.ru>
2018-06-12 15:45:26 +0300

    add dev server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Test from './test';
+import Test2 from './test2';
 
 render(
     <div>
         Hello world!
+        <Test />
+        <Test2 />
     </div>,
     document.getElementById('root')
 )
+
+//fix reload page
+module.hot.accept()
